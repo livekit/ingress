0bd9391496123fb9aacb127fcf368b9fd00718d2
Benjamin Pracht <benjamin@livekit.io>
2023-03-17 15:50:37 -0700

    WiP

---
+++ pkg/media/presets.go
@@ -0,0 +1,46 @@
+package media
+
+import (
+	"github.com/livekit/ingress/pkg/errors"
+	"github.com/livekit/protocol/livekit"
+)
+
+func getOptionsForVideoPreset(preset livekit.IngressVideoEncodingPreset) (*livekit.IngressVideoEncodingOptions, error) {
+	switch preset {
+	case livekit.IngressVideoEncodingPreset_H264_720P_30FPS_3_LAYERS:
+		return &livekit.IngressVideoEncodingOptions{
+			VideoCodec: livekit.VideoCodec_H264_BASELINE,
+			FrameRate:  30,
+			Layers: computeVideoLayers(&livekit.VideoLayer{
+				Quality: livekit.VideoQuality_HIGH,
+				Width:   1280,
+				Height:  720,
+				Bitrate: 1700000,
+			}, 3),
+		}, nil
+	default:
+		return nil, errors.ErrInvalidVideoPreset
+	}
+}
+
+func computeVideoLayers(highLayer *livekit.VideoLayer, layerCount int) []*livekit.VideoLayer {
+	layerCopy := *highLayer
+	layerCopy.Quality = livekit.VideoQuality_HIGH
+
+	layers := []*livekit.VideoLayer{
+		&layerCopy,
+	}
+
+	for i := 1; i < layerCount; i++ {
+		layer := &livekit.VideoLayer{
+			Width:   layerCopy.Width >> i, // each layer has dimentions half of the previous one
+			Height:  layerCopy.Height >> i,
+			Bitrate: layerCopy.Bitrate >> (2 * i), // bitrate 1/4 of the previous layer
+			Quality: livekit.VideoQuality(layerCount - 1 - i),
+		}
+
+		layers = append(layers, layer)
+	}
+
+	return layers
+}
