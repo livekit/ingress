8a8323ad48af5f947cddfdb7472d030f3883e86c
Benjamin Pracht <benjamin@livekit.io>
2023-04-07 11:15:04 -0700

    docker tests

--- build/test/entrypoint.sh
+++ build/test/entrypoint.sh
@@ -4,8 +4,8 @@ set -exo pipefail
 
 # Run tests
 if [[ -z "${GITHUB_WORKFLOW}" ]]; then
-  exec ./test.test -test.v -test.timeout 20m
+  exec go test -v -timeout 20m ./pkg/...
 else
   go install github.com/gotesttools/gotestfmt/v2/cmd/gotestfmt@latest
-  exec go tool test2json -p ingress ./test.test -test.v -test.timeout 20m 2>&1 | "$HOME"/go/bin/gotestfmt
+  exec go tool test2json -p ingress go test -test.v=true -test.timeout 20m ./pkg/... 2>&1 | "$HOME"/go/bin/gotestfmt
 fi
