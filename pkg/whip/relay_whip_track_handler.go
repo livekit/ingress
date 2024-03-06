95895d617374290017b9aa0405ad887571322e02
Benjamin Pracht <benjamin@livekit.io>
2024-03-06 13:09:00 -0800

    WiP

--- pkg/whip/relay_whip_track_handler.go
+++ pkg/whip/relay_whip_track_handler.go
@@ -1,3 +1,17 @@
+// Copyright 2023 LiveKit, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package whip
 
 import (
@@ -32,7 +46,6 @@ type RelayWhipTrackHandler struct {
 	depacketizer rtp.Depacketizer
 	quality      livekit.VideoQuality
 	receiver     *webrtc.RTPReceiver
-	mediaSink    MediaSink
 	sync         *synchronizer.TrackSynchronizer
 	writePLI     func(ssrc webrtc.SSRC)
 	onRTCP       func(packet rtcp.Packet)
@@ -54,7 +67,6 @@ func NewRelayWhipTrackHandler(
 	track *webrtc.TrackRemote,
 	quality livekit.VideoQuality,
 	sync *synchronizer.TrackSynchronizer,
-	mediaSink MediaSink,
 	receiver *webrtc.RTPReceiver,
 	writePLI func(ssrc webrtc.SSRC),
 	onRTCP func(packet rtcp.Packet),
@@ -108,8 +120,6 @@ func (t *whipTrackHandler) SetMediaTrackStatsGatherer(st *stats.LocalMediaStatsG
 	t.trackStats = g
 
 	t.statsLock.Unlock()
-
-	t.mediaSink.SetStatsGatherer(st)
 }
 
 func (t *whipTrackHandler) Close() {
