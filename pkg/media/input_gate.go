266504f364157da151e731f85700e4d6876cc02e
Milos Pesic <milos.pesic@livekit.io>
2025-11-17 21:39:51 +0100

    Simplifying event probe

--- pkg/media/input_gate.go
+++ pkg/media/input_gate.go
@@ -99,51 +99,28 @@ func (i *Input) addGateProbe(pad *gst.Pad, padName string, state *padTimingState
 }
 
 func (i *Input) addSegmentEventProbe(pad *gst.Pad, padName string, state *padTimingState) {
-	logSegmentEvent := func(direction string, seg *gst.Segment) {
-		if seg == nil {
-			logger.Debugw("nil segment event received", "pad", padName, "direction", direction)
-			return
-		}
-
-		fields := []interface{}{
-			"pad", padName,
-			"direction", direction,
-			"format", seg.GetFormat(),
-			"rate", seg.GetRate(),
-			"appliedRate", seg.GetAppliedRate(),
-			"base", seg.GetBase(),
-			"start", seg.GetStart(),
-			"stop", seg.GetStop(),
-			"time", seg.GetTime(),
-			"position", seg.GetPosition(),
-		}
-
-		if seg.GetFormat() == gst.FormatTime {
-			fields = append(fields,
-				"baseDur", time.Duration(seg.GetBase()),
-				"startDur", time.Duration(seg.GetStart()),
-				"stopDur", time.Duration(seg.GetStop()),
-				"timeDur", time.Duration(seg.GetTime()),
-				"positionDur", time.Duration(seg.GetPosition()),
-			)
-		}
-
-		state.segmentStart.Store(time.Duration(seg.GetStart()))
-
-		logger.Debugw("segment event received", fields...)
-	}
-
 	pad.AddProbe(gst.PadProbeTypeEventDownstream, func(_ *gst.Pad, info *gst.PadProbeInfo) gst.PadProbeReturn {
 		event := info.GetEvent()
-		if event == nil {
+		if event == nil || event.Type() != gst.EventTypeSegment {
 			return gst.PadProbeOK
 		}
 
-		switch event.Type() {
-		case gst.EventTypeSegment:
-			logSegmentEvent("downstream", event.ParseSegment())
+		seg := event.ParseSegment()
+		if seg == nil {
+			logger.Debugw("segment event missing data", "pad", padName)
+			return gst.PadProbeOK
 		}
 
+		state.segmentStart.Store(time.Duration(seg.GetStart()))
+		logger.Debugw("segment event received",
+			"pad", padName,
+			"format", seg.GetFormat(),
+			"start", seg.GetStart(),
+			"startDur", time.Duration(seg.GetStart()),
+			"time", seg.GetTime(),
+			"timeDur", time.Duration(seg.GetTime()),
+			"rate", seg.GetRate(),
+		)
 		return gst.PadProbeOK
 	})
 }
