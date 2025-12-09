2f8b0ecda03d702a06b3d0eaa2d1c2ce9b61e723
Milos Pesic <milos.pesic@livekit.io>
2025-12-09 11:50:25 +0100

    Revert "Add a nil check to reportJitterStats (#404)" (#405)
    This reverts commit 605ed4bc1906b2d578e2d2d15094f7b82353764d.

--- pkg/whip/relay_whip_track_handler.go
+++ pkg/whip/relay_whip_track_handler.go
@@ -25,6 +25,8 @@ import (
 	"go.uber.org/atomic"
 
 	"github.com/frostbyte73/core"
+	"github.com/livekit/ingress/pkg/errors"
+	"github.com/livekit/ingress/pkg/stats"
 	"github.com/livekit/media-sdk/jitter"
 	"github.com/livekit/protocol/livekit"
 	"github.com/livekit/protocol/logger"
@@ -33,9 +35,6 @@ import (
 	"github.com/pion/rtp"
 	"github.com/pion/webrtc/v4"
 	"github.com/pion/webrtc/v4/pkg/media"
-
-	"github.com/livekit/ingress/pkg/errors"
-	"github.com/livekit/ingress/pkg/stats"
 )
 
 const (
@@ -335,10 +334,6 @@ func (t *RelayWhipTrackHandler) reportJitterStats() {
 	t.statsLock.Lock()
 	g := t.trackStats
 	t.statsLock.Unlock()
-
-	if g == nil {
-		return
-	}
 
 	jbStats := t.jb.Stats()
 	if jbStats == nil {
